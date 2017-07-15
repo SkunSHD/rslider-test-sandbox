900a5af63111f8d2ec99f4676ef570d2daec4261
malyutenko.d <malyutenko.d@gmail.com>
2017-07-15 15:34:06 +0300

    Can't find react module

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { observable } from 'mobx';
 
 import { observer } from 'mobx-react';
@@ -6,12 +6,24 @@ import DevTools from 'mobx-react-devtools';
 import ReactDOM from 'react-dom';
 
 import rSlider from 'rslider';
+const {RSlider, RSliderArrowL, RSliderArrowR, RSliderItems, RSliderPagination} = rSlider;
+
 
 @observer
 class App extends Component {
   render() {
     return (
       <div>
+      <RSlider name="example1">
+        <RSliderItems>
+              <div>item 1</div>
+              <div>item 2</div>
+          </RSliderItems>
+   
+          <RSliderArrowL />
+          <RSliderArrowR />
+          <RSliderPagination />
+      </RSlider>
         <button onClick={this.onReset}>
           Seconds passed: {this.props.appState.timer}
         </button>
